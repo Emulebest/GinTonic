cb0119ac2faa62485e4e9109c6f4b8050b41dee6
Diana Baburina <dianababurina74@gmail.com>
2018-05-09 15:00:11 +0300

    add base makup of lending and header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,96 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react';
 import {connect} from "react-redux";
 import {withRouter} from "react-router";
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+import "../style/Header.css"
 
 const mapStateToProps = (state) => ({
     token: state.auth.login.token
 });
 
-class Header extends Component {
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
 
     render() {
         let token = this.props.token || localStorage.getItem('token');
         return (
-            <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/register'>Sign up</Link></li>
-                    {
-                        (!token) ?
-                            (<li>
-                                <Link to='/login'>Sign in</Link>
-                            </li>)
-                            :
-                            (<React.Fragment>
-                                <li><Link to='/login'>Profile</Link></li>
-                                <li><Link to='/logout'>Logout</Link></li>
-                            </React.Fragment>)
-                    }
-                </ul>
-            </nav>
-        )
+            <React.Fragment>
+                <Navbar expand="md">
+                    <NavbarBrand href="/">Home</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="#">Concept</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Features</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Get started</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="/register">Sign up</NavLink>
+                            </NavItem>
+                            {
+                                (!token) ?
+                                    (<NavItem>
+                                        <NavLink href="/login">Sign in</NavLink>
+                                    </NavItem>)
+                                    :
+                                    (<React.Fragment>
+                                        <UncontrolledDropdown nav inNavbar>
+                                            <DropdownToggle nav caret>
+                                                Account
+                                            </DropdownToggle>
+                                            <DropdownMenu right>
+                                                <DropdownItem>
+                                                    <NavLink href="#">My devices</NavLink>
+                                                </DropdownItem>
+                                                <DropdownItem>
+                                                    <NavLink href="#">Settings</NavLink>
+                                                </DropdownItem>
+                                                <DropdownItem divider/>
+                                                <DropdownItem>
+                                                    <NavLink href="/logout">Logout</NavLink>
+                                                </DropdownItem>
+                                            </DropdownMenu>
+                                        </UncontrolledDropdown>
+                                    </React.Fragment>)
+                            }
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </React.Fragment>
+        );
     }
 }
 
