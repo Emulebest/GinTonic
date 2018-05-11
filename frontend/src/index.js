a47df5755379002ba2fd40b9193e984a1d3dc140
Diana Baburina <dianababurina74@gmail.com>
2018-05-11 20:28:56 +0300

    delete files

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import router from "./router";
-import 'bootstrap/dist/css/bootstrap.css';
-import "./style/index.css";
-
-ReactDOM.render(router, document.getElementById('root'));
