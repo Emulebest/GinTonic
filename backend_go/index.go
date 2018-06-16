9ba2adb6a4c2021728d02fd39fdc90943051e31a
Vadym Kaidalov <vadym.kaidalov@nure.ua>
2018-06-16 13:13:33 +0300

    Setting Access-Control-Allow-Origin header to * and other stuff.

--- backend_go/index.go
+++ backend_go/index.go
@@ -6,12 +6,29 @@ import (
 	"./middlewares"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/cors"
 )
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Auth")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 
 func initializeRoutes() *gin.Engine {
 
 	router := gin.Default()
+
+	router.Use(CORSMiddleware())
 
 	userRoutes := router.Group("/user")
 	{
@@ -36,12 +53,12 @@ func main() {
 	fmt.Println("[VADYM] Successfully connected to the Postgres DB.")
 
 	router := initializeRoutes()
-
+/*
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Auth"},
 		AllowCredentials: true,
 	}))
-
+*/
 	router.Run()
 }
