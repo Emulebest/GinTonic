3e60f07b149c44f5583ecf866e39821f9e0d7fcb
DiannaDea <dianababurina74@gmail.com>
2018-06-17 18:47:49 +0300

    add transaction history2

--- frontend/src/sagas/payment/history.js
+++ frontend/src/sagas/payment/history.js
@@ -8,29 +8,29 @@ import {getHistorySuccess, getHistoryFailure} from "../../actions/transactions/h
 export default function* getHistoryTransactions({payload}) {
     try {
 
-        // const res = yield call(axios, {
-        //     url: `${BLOCKCHAIN_URL}/history`,
-        //     method: "GET",
-        //     headers: {
-        //         "Accept": "application/json",
-        //         "Content-Type": "application/json"
-        //     }
-        // });
+        const res = yield call(axios, {
+            url: `${BLOCKCHAIN_URL}/history`,
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+        });
 
 
-        let res = {
-            "transactions": [{
-                "from": "N8QowfrX6p",
-                "to": "C9DpZPLMbW",
-                "amount": "20",
-                "private": "ab"
-            }, {"from": "N8QowfrX6p", "to": "C9DpZPLMbW", "amount": "20", "private": "ab"}, {
-                "from": "N8QowfrX6p",
-                "to": "C9DpZPLMbW",
-                "amount": "20",
-                "private": "ab"
-            }, {"from": "N8QowfrX6p", "to": "C9DpZPLMbW", "amount": "20", "private": "ab"}]
-        };
+        // let res = {
+        //     "transactions": [{
+        //         "from": "N8QowfrX6p",
+        //         "to": "C9DpZPLMbW",
+        //         "amount": "20",
+        //         "private": "ab"
+        //     }, {"from": "N8QowfrX6p", "to": "C9DpZPLMbW", "amount": "20", "private": "ab"}, {
+        //         "from": "N8QowfrX6p",
+        //         "to": "C9DpZPLMbW",
+        //         "amount": "20",
+        //         "private": "ab"
+        //     }, {"from": "N8QowfrX6p", "to": "C9DpZPLMbW", "amount": "20", "private": "ab"}]
+        // };
 
         yield put(getHistorySuccess(res.transactions));
 
