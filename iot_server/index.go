c56bd754eb78cab939e4897c35546799e12e5e40
igorkorotach <igorkorotach@gmail.com>
2018-06-17 20:30:58 +0300

    iot config

--- iot_server/index.go
+++ iot_server/index.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
- firmataAdaptor := firmata.NewTCPAdaptor("192.168.0.253:3030")
+ firmataAdaptor := firmata.NewTCPAdaptor("192.168.0.254:3030")
  led := gpio.NewLedDriver(firmataAdaptor, "2")
 
  mbot := gobot.NewMaster()
