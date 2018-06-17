007df46a48d5d3aeda73b5b374dbcc81dca70b4d
DiannaDea <dianababurina74@gmail.com>
2018-06-17 14:40:00 +0300

    add wallet

--- frontend/src/constants/baseUrl.js
+++ frontend/src/constants/baseUrl.js
@@ -2,3 +2,4 @@
 //export const BASE_URL: string = "http://localhost:3000";
 
 export const BASE_URL: string = "http://localhost:8080";
+export const BLOCKCHAIN_URL : string = "http://localhost:8081";
